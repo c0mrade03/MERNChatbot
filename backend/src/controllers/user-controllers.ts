896b2ce6e35d22bc6b1ec4c8b7974a7e878d8e97
Prathamesh <devpsk03@gmail.com>
2024-03-03 16:52:00 +0530

    wrote a user-controler with some beginner's logic

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js"
+import { hash } from "bcrypt";
 
 export const getAllUsers = async (
   req: Request,
@@ -9,6 +10,30 @@ export const getAllUsers = async (
   try {
     const users = await User.find();
     res.status(200).json({ msg: "OK", users });
+  }
+  catch (error) {
+    res.status(200).json({
+      msg: "ERROR", Cause: error
+    })
+  }
+}
+
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await hash(password, 10);
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword
+    })
+    await user.save();
+
+    res.status(200).json({ message: "OK", id: user._id.toString() });
   }
   catch (error) {
     res.status(200).json({
